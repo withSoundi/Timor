96141127d12d03c8c8acf00d8b3645ff3ab99b9b
Soundi <Soundi@github.com>
2019-11-29 13:41:13 +0800

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by 姜天姣 on 2019/11/29.
+//  Copyright © 2019 姜天姣. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
